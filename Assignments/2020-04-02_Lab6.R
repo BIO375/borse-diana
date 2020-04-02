dc1bf579d7e7c7b4e442196c6b8e2327ae18df13
Diana-Borse <bella.borse@gmail.com>
2020-04-02 10:17:11 -0500

    Ran into a problem

--- Assignments/2020-04-02_Lab6.R
+++ Assignments/2020-04-02_Lab6.R
@@ -22,3 +22,63 @@ tidyverse_update()
 library(readr)
 Craig_Foote <- read_csv("~/Bio 375/Analyses/borse-diana/datasets/demos/Craig_Foote.csv")
 
+# Calculate summary statistics
+summ_Craig_Foote <- Craig_Foote %>%
+  group_by(Type) %>%
+  summarise(mean_Color = mean(Color),
+            median_Color = median(Color),
+            IQR_Color = IQR(Color),
+            sd_Color = sd(Color),
+            var_Color = var(Color),
+            se_Color = sd(Color)/sqrt(n()),
+            n_Color = n()) 
+
+view(summ_Craig_Foote)
+
+# Checking Normality of distributions
+
+ggplot(Craig_Foote) +
+  geom_histogram(aes(Color), binwidth = .1)+
+  facet_wrap(~Type)
+
+# Testing whether the variances are similar enough
+
+ratio <- (max(summ_Craig_Foote$sd_Color))/(min(summ_Craig_Foote$sd_Color))
+
+view(ratio)
+
+# Trying a transformation
+
+Craig_Foote <- Craig_Foote %>%
+  mutate(logColor = log(Color))
+
+summ_Craig_Foote <- Craig_Foote %>%
+  group_by(Type) %>%
+  summarise(mean_logColor = mean(logColor),
+            median_logColor = median(logColor),
+            IQR_logColor = IQR(logColor),
+            sd_logColor = sd(logColor),
+            var_logColor = var(logColor),
+            se_logColor = sd(logColor)/sqrt(n()),
+            n_logColor = n()) 
+
+#Checking Normality
+
+ggplot(Craig_Foote) +
+  geom_histogram(aes(logColor), binwidth = .05)+
+  facet_wrap(~Type)
+
+ggplot(Craig_Foote) +
+  geom_boxplot(aes(x = Type, y = logColor))
+
+# Check if the ratio of sdmax to sdmin is acceptable
+
+ratio <- (max(summ_Craig_Foote$sd_logColor))/(min(summ_Craig_Foote$sd_logColor))
+
+view(ratio)
+
+# Perform t-test (Mann Whitney U / Wilcox)
+
+Wilcox.test(Color ~ Type, data = Craig_Foote, var.equal = TRUE, alternative = "less", mu = 0, conf.level = 0.95)
+
+
