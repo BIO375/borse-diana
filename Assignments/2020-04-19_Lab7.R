5f69adfd3e3d0ff44a4894ced5f4dfaf6be6a4bb
Diana-Borse <bella.borse@gmail.com>
2020-04-14 16:09:42 -0500

    Hadn't saved this yet

---
+++ Assignments/2020-04-19_Lab7.R
@@ -0,0 +1,24 @@
+#### Lab 7: Introduction to 1-way ANOVA ####
+
+# Clean up working environment
+rm(list = ls())
+# Verify working directory
+getwd()
+
+# Install package ggfortify, *note* only do install.packages ONCE
+# ggfortify is a package that works with ggplot2 to make nice plots
+# install.packages("ggfortify")
+library("ggfortify")
+# multcomp is used for contrasts and multiple comparisons
+# install.packages("multcomp")
+library("multcomp")
+# nlme is used for random effects ANOVA
+# install.packages("nlme")
+library("nlme")
+
+# Load tidyverse
+library("tidyverse")
+# Check for updates
+tidyverse_update()
+
+#### Question 1 ####
