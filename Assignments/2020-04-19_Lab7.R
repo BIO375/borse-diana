4da84e48e34ba9d48cd6fd28f37cda8e82f5e4dc
Diana-Borse <bella.borse@gmail.com>
2020-04-14 17:09:51 -0500

    Progress

--- Assignments/2020-04-19_Lab7.R
+++ Assignments/2020-04-19_Lab7.R
@@ -22,3 +22,73 @@ library("tidyverse")
 tidyverse_update()
 
 #### Question 1 ####
+
+# Load data
+Jaffe <- read_csv("datasets/demos/Jaffe.csv")
+
+# Do the necessary transformation for ANOVA
+Jaffe <- Jaffe %>%
+  mutate(Depth = fct_recode(Depth, Surface = "Surface",
+                               Middepth = "Middepth",
+                              Bottom = "Bottom"))
+# Look at it
+
+head(Jaffe)
+summary(Jaffe)
+
+# Plots to check if assumption of normality is met for Aldrin concentration
+ggplot(Jaffe, aes(x = Depth, y = Aldrin))+
+  geom_boxplot() +
+  theme_bw() +
+  coord_flip()
+ggplot(Jaffe) +
+  geom_histogram(aes(Aldrin), binwidth = 1)+
+  facet_wrap(~Depth)
+ggplot(Jaffe)+
+  geom_qq(aes(sample = Aldrin, color = Depth))
+
+# Plots to check if the assumption of normality is met from HCB concentration
+ggplot(Jaffe, aes(x = Depth, y = HCB))+
+  geom_boxplot() +
+  theme_bw() +
+  coord_flip()
+ggplot(Jaffe) +
+  geom_histogram(aes(HCB), binwidth = 1)+
+  facet_wrap(~Depth)
+ggplot(Jaffe)+
+  geom_qq(aes(sample = HCB)) +
+          facet_wrap(~Depth)
+
+# Normailty is not met for Aldrin concentration
+
+# Must transform:
+Jaffe <- Jaffe %>%
+  mutate(ln_Aldrin = log(Aldrin))
+
+# Try checking for normality again
+ggplot(Jaffe, aes(x = Depth, y = ln_Aldrin))+
+  geom_boxplot() +
+  theme_bw() +
+  coord_flip()
+ggplot(Jaffe) +
+  geom_histogram(aes(ln_Aldrin), binwidth = .5)+
+  facet_wrap(~Depth)
+ggplot(Jaffe)+
+  geom_qq(aes(sample = ln_Aldrin)) +
+  facet_wrap(~Depth)
+
+# Testing for homogeneous variance of Aldrin
+summ_Aldrin <- Jaffe %>%
+  group_by(Depth) %>% 
+  summarise(mean_Aldrin = mean(Aldrin),
+            sd_Aldrin = sd(Aldrin),
+            n_Aldrin = n())
+ratio <-(max(summ_Aldrin$sd_Aldrin))/(min(summ_Aldrin$sd_Aldrin))
+
+# Testing for homogeneous variance of HCB
+summ_HCB <- Jaffe %>%
+  group_by(Depth) %>% 
+  summarise(mean_HCB = mean(HCB),
+            sd_HCB = sd(HCB),
+            n_HCB = n())
+ratio <-(max(summ_HCB$sd_HCB))/(min(summ_HCB$sd_HCB))
