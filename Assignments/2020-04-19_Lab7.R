e7c5bbdf707a68b45317bdc2bed0ae8827f7f302
Diana-Borse <bella.borse@gmail.com>
2020-04-20 13:56:35 -0500

    Finished

--- Assignments/2020-04-19_Lab7.R
+++ Assignments/2020-04-19_Lab7.R
@@ -38,48 +38,24 @@ summary(Jaffe)
 
 #### Testing assumptions ####
 
-# Plots to check if assumption of normality is met for Aldrin concentration
-ggplot(Jaffe, aes(x = Depth, y = Aldrin))+
-  geom_boxplot() +
-  theme_bw() +
-  coord_flip()
+# Normality Aldrin
+ggplot(Jaffe) +
+  geom_boxplot(aes(x = Depth, y = Aldrin))+
+  stat_summary(aes(x = Depth, y = Aldrin), 
+               fun.y=mean, 
+               colour="blue", 
+               fill = "blue",
+               geom="point", 
+               shape=21, 
+               size=3)
 ggplot(Jaffe) +
   geom_histogram(aes(Aldrin), binwidth = 1)+
   facet_wrap(~Depth)
 ggplot(Jaffe)+
   geom_qq(aes(sample = Aldrin, color = Depth))
 
-# Plots to check if the assumption of normality is met from HCB concentration
-ggplot(Jaffe, aes(x = Depth, y = HCB))+
-  geom_boxplot() +
-  theme_bw() +
-  coord_flip()
-ggplot(Jaffe) +
-  geom_histogram(aes(HCB), binwidth = 1)+
-  facet_wrap(~Depth)
-ggplot(Jaffe)+
-  geom_qq(aes(sample = HCB)) +
-          facet_wrap(~Depth)
+# Homogeneous variance
 
-# Normailty is not met for Aldrin concentration
-
-# Must transform:
-Jaffe <- Jaffe %>%
-  mutate(ln_Aldrin = log(Aldrin))
-
-# Try checking for normality again
-ggplot(Jaffe, aes(x = Depth, y = ln_Aldrin))+
-  geom_boxplot() +
-  theme_bw() +
-  coord_flip()
-ggplot(Jaffe) +
-  geom_histogram(aes(ln_Aldrin), binwidth = .5)+
-  facet_wrap(~Depth)
-ggplot(Jaffe)+
-  geom_qq(aes(sample = ln_Aldrin)) +
-  facet_wrap(~Depth)
-
-# Testing for homogeneous variance of Aldrin
 summ_Aldrin <- Jaffe %>%
   group_by(Depth) %>% 
   summarise(mean_Aldrin = mean(Aldrin),
@@ -87,22 +63,63 @@ summ_Aldrin <- Jaffe %>%
             n_Aldrin = n())
 ratio <-(max(summ_Aldrin$sd_Aldrin))/(min(summ_Aldrin$sd_Aldrin))
 
-# Test for transformed data
+# Plots to check if the assumption of normality is met from HCB concentration
+ggplot(Jaffe) +
+  geom_boxplot(aes(x = Depth, y = HCB))+
+  stat_summary(aes(x = Depth, y = HCB), 
+               fun.y=mean, 
+               colour="blue", 
+               fill = "blue",
+               geom="point", 
+               shape=21, 
+               size=3)
+ggplot(Jaffe) +
+  geom_histogram(aes(HCB), binwidth = 1)+
+  facet_wrap(~Depth)
+ggplot(Jaffe)+
+  geom_qq(aes(sample = HCB)) +
+          facet_wrap(~Depth)
 
-summ_Aldrin2 <- Jaffe %>%
-  group_by(Depth) %>% 
-  summarise(mean_ln_Aldrin = mean(ln_Aldrin),
-            sd_ln_Aldrin = sd(ln_Aldrin),
-            n_ln_Aldrin = n())
-ratio <-(max(summ_Aldrin2$sd_ln_Aldrin))/(min(summ_Aldrin2$sd_ln_Aldrin))
-
-# Testing for homogeneous variance of HCB
+# Checking for homogeneous variance HCB
 summ_HCB <- Jaffe %>%
   group_by(Depth) %>% 
   summarise(mean_HCB = mean(HCB),
             sd_HCB = sd(HCB),
             n_HCB = n())
 ratio <-(max(summ_HCB$sd_HCB))/(min(summ_HCB$sd_HCB))
+
+# Normailty is met for both
+# HCB has homogeneous variance
+# Aldrin does not have homogeneous variance
+
+# Must transform Aldrin:
+Jaffe <- Jaffe %>%
+  mutate(ln_Aldrin = log10(Aldrin))
+
+# Try checking for normality again
+ggplot(Jaffe) +
+  geom_boxplot(aes(x = Depth, y = ln_Aldrin))+
+  stat_summary(aes(x = Depth, y = ln_Aldrin), 
+               fun.y=mean, 
+               colour="blue", 
+               fill = "blue",
+               geom="point", 
+               shape=21, 
+               size=3)
+ggplot(Jaffe) +
+  geom_histogram(aes(ln_Aldrin), binwidth = .15)+
+  facet_wrap(~Depth)
+ggplot(Jaffe)+
+  geom_qq(aes(sample = ln_Aldrin)) +
+  facet_wrap(~Depth)
+
+# Testing for homogeneous variance of Aldrin
+summ_ln_Aldrin <- Jaffe %>%
+  group_by(Depth) %>% 
+  summarise(mean_ln_Aldrin = mean(ln_Aldrin),
+            sd_ln_Aldrin = sd(ln_Aldrin),
+            n_ln_Aldrin = n())
+ratio <-(max(summ_ln_Aldrin$sd_ln_Aldrin))/(min(summ_ln_Aldrin$sd_ln_Aldrin))
 
 #### Performing the ANOVA for Aldrin concentration ####
 
