8591b6187929a703585bcf2b564bd4999624530d
Sarah Lee <scrislee@gmail.com>
2020-04-30 10:21:39 -0400

    lab keys

--- Assignments/2020-03-12_Lab5.R
+++ Assignments/2020-03-12_Lab5.R
@@ -227,3 +227,4 @@ t.test(elgar$HORIZDIM, elgar$HORIZLIG,
 
 t.test(elgar$HORIZLIG, elgar$HORIZDIM,
         alternative = "two.sided", paired = TRUE, conf.level = 0.95)
+### 10/10 code runs without breaking ####
